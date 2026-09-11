19b4d1b8fa8336bcf1a107ddb015c069b958974d
ludovic-laloux <ludovic.laloux@outlook.com>
2026-09-11 22:57:46 +0200

    Add pause menu

--- code/main.py
+++ code/main.py
@@ -14,7 +14,7 @@ class Game:
 		# health and score setup
 		self.lives = 3
 		self.live_surf = pygame.image.load('../graphics/player.png').convert_alpha()
-		self.live_x_start_pos = screen_width - (self.live_surf.get_size()[0] * 2 + 20)
+		self.live_x_start_pos = screen_width - (self.live_surf.get_size()[0] * 2 + 10) - 10
 		self.score = 0
 		self.font = pygame.font.Font('../font/Pixeled.ttf',20)
 
@@ -37,14 +37,17 @@ class Game:
 		self.extra_spawn_time = randint(40,80)
 
 		# Audio
-		music = pygame.mixer.Sound('../audio/music.wav')
-		music.set_volume(0.2)
-		music.play(loops = -1)
+		self.music = pygame.mixer.Sound('../audio/music.wav')
+		self.music.set_volume(0.2)
+		self.music.play(loops = -1)
 		self.laser_sound = pygame.mixer.Sound('../audio/laser.wav')
 		self.laser_sound.set_volume(0.1)
 		self.explosion_sound = pygame.mixer.Sound('../audio/explosion.wav')
 		self.explosion_sound.set_volume(0.3)
 		self.damage_sound = pygame.mixer.Sound("../audio/damage.wav")
+
+		# UI setup
+		self.paused = False
 
 	def create_obstacle(self, x_start, y_start,offset_x):
 		for row_index, row in enumerate(self.shape):
@@ -157,9 +160,23 @@ class Game:
 
 	def victory_message(self):
 		if not self.aliens.sprites():
-			victory_surf = self.font.render('You won',False,'white')
+			victory_font = pygame.font.Font("../font/pixeled.ttf",30)
+			victory_surf = victory_font.render('You won',False,'white')
 			victory_rect = victory_surf.get_rect(center = (screen_width / 2, screen_height / 2))
 			screen.blit(victory_surf,victory_rect)
+
+	def pause_menu(self):
+		pause_surf = self.font.render("PAUSED", False,"white")
+		pause_rect = pause_surf.get_rect(center = (screen_width/2,screen_height/2 - 30))
+		screen.blit(pause_surf,pause_rect)
+
+		resume_surf = self.font.render("Press Enter to resume",False,"white")
+		resume_rect = resume_surf.get_rect(center = (screen_width/2,screen_height/2 + 30))
+		screen.blit(resume_surf,resume_rect)
+
+		quit_surf = self.font.render("Press Esc to quit",False,"white")
+		quit_rect = quit_surf.get_rect(center = (screen_width/2,screen_height/2 + 90))
+		screen.blit(quit_surf,quit_rect)
 
 	def run(self):
 		self.player.update()
@@ -216,14 +233,23 @@ if __name__ == '__main__':
 			if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
 				pygame.quit()
 				sys.exit()
+
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				sys.exit()
-			if event.type == ALIENLASER:
+
+			if event.type == ALIENLASER and not game.paused:
 				game.alien_shoot()
 
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+				game.paused = not game.paused
+
+				if game.paused: game.music.stop()
+				else: game.music.play(loops = -1)
+
 		screen.fill((30,30,30))
-		game.run()
+		if not game.paused: game.run()
+		else: game.pause_menu()
 		crt.draw()
 			
 		pygame.display.flip()
